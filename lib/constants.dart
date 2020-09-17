f8a6505d4fe89cfa60602d38c19c4d54163b30cc
unknown <farhad.salehi.21@gmail.com>
2020-09-17 18:25:37 +0430

    first

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,4 +1,4 @@
 import 'package:flutter/material.dart';
 
-const kPrimaryColor = Color(0xFF51c2bf);
-const kPrimaryLightColor = Color(0xFF3d91b9);
+const kPrimaryColor = Color(0xFF6F35A5);
+const kPrimaryLightColor = Color(0xFFF1E6FF);
